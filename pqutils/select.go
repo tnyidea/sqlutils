f31eb8a198ac824637d76952e2d86aa983e6e013
gbnyc26 <gregbehrend26@gmail.com>
2021-10-20 13:02:41 -0400

    Modified select to match delete and update

--- pqutils/select.go
+++ pqutils/select.go
@@ -8,30 +8,51 @@ import (
 	"strings"
 )
 
-func SelectOne(db *sql.DB, table string, schemaType interface{}, where map[string]string) (interface{}, error) {
-	result, err := SelectAllWithOptions(db, table, schemaType, where, QueryOptions{Limit: 1})
+func SelectOne(db *sql.DB, table string, v interface{}) (interface{}, error) {
+	err := checkKindStruct(v)
 	if err != nil {
 		return nil, err
 	}
 
+	sm := parseSchemaTypeValue(&v)
+
+	var where map[string]string
+	for columnName, keyType := range sm.columnKeyTypeMap {
+		if strings.Contains(keyType, "primarykey") {
+			if where == nil {
+				where = make(map[string]string)
+			}
+			fieldName := sm.columnNameFieldNameMap[columnName]
+			where[fieldName] = sm.fieldNameStringValueMap[fieldName]
+		}
+	}
+
+	// Test for uniqueness, if valid should only have one record that matches
+	emptyResult := reflect.New(reflect.ValueOf(v).Type()).Elem().Interface()
+	result, err := selectAllWithOptions(db, table, v, where, QueryOptions{})
+	if err != nil {
+		return emptyResult, err
+	}
 	if len(result) != 1 {
-		return reflect.New(reflect.ValueOf(schemaType).Type()).Elem().Interface(), nil
+		return emptyResult, errors.New("not found: cannot find unique value for primary key values of v")
 	}
 
 	return result[0], nil
 }
 
 func SelectAll(db *sql.DB, table string, schemaType interface{}) ([]interface{}, error) {
-	if !reflect.ValueOf(schemaType).IsZero() {
-		return nil, errors.New("invalid schemaType: must be a zero-value struct")
-	}
-	return SelectAllWithOptions(db, table, schemaType, nil, QueryOptions{})
+	return selectAllWithOptions(db, table, schemaType, nil, QueryOptions{})
 }
 
-func SelectAllWithOptions(db *sql.DB, table string, schemaType interface{},
+func SelectAllWithOptions(db *sql.DB, table string, schemaType interface{}, where map[string]string, options QueryOptions) ([]interface{}, error) {
+	return selectAllWithOptions(db, table, schemaType, where, options)
+}
+
+func selectAllWithOptions(db *sql.DB, table string, schemaType interface{},
 	where map[string]string, options QueryOptions) ([]interface{}, error) {
 
 	sm := parseSchemaTypeValue(&schemaType)
+
 	query := `SELECT ` + strings.Join(sm.columnNames, ", ") + `
 		FROM ` + table +
 		whereConditionString(schemaType, where) +
@@ -65,37 +86,6 @@ func SelectAllWithOptions(db *sql.DB, table string, schemaType interface{},
 	// Collect the results
 	var result []interface{}
 	for rows.Next() {
-		// Scan the row result
-		// TODO KEEP THIS HERE -- maybe we don't need to reuse this as we build
-		//var sd []interface{}
-		//for range columnTypes {
-		//	// TODO add code for the array test case when we get an array back from postgres
-		//	// sd = append(sd, pq.Array(field.Addr().Interface()))
-		//	var v interface{}
-		//	sd = append(sd, &v)
-		//}
-		//err := rows.Scan(sd...)
-		//if err != nil {
-		//    return nil, err
-		//}
-		//
-		//rowResult := reflect.New(reflect.ValueOf(schemaType).Type())
-		//log.Println(rowResult.Elem())
-		//for i, columnType := range columnTypes {
-		//    columnName := columnType.Name()
-		//	log.Println(columnName)
-		//    columnTypeName := columnType.DatabaseTypeName()
-		//    switch columnTypeName {
-		//    case "INT4":
-		//        log.Println(*sd[i].(*interface{}))
-		//		rowResult.Elem().FieldByName(columnFieldMap[columnName]).SetInt((*sd[i].(*interface{})).(int64))
-		//    case "VARCHAR":
-		//		log.Println(*sd[i].(*interface{}))
-		//		rowResult.Elem().FieldByName(columnFieldMap[columnName]).SetString((*sd[i].(*interface{})).(string))
-		//    default:
-		//        return nil, errors.New("scan error: unhandled type: " + columnTypeName)
-		//    }
-		//}
 		rowResult, err := unmarshalRowsResult(rows, columnTypes, schemaType, sm)
 		if err != nil {
 			return nil, err
